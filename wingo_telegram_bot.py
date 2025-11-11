82abd8c27f21169daa6b135b2935b34084f5f8ba
riteshlbs <riteshlbs2022@gmail.com>
2025-11-11 22:55:46 +0530

    Update wingo_telegram_bot.py

--- wingo_telegram_bot.py
+++ wingo_telegram_bot.py
@@ -2,8 +2,8 @@ import telebot
 from collections import deque
 import os
 
-BOT_TOKEN = 7677423658:AAH-F8AP6XoZrleihOjjAXROzEEp-mq7_xA
-bot = 7677423658:AAH-F8AP6XoZrleihOjjAXROzEEp-mq7_xA
+BOT_TOKEN = os.getenv "7677423658:AAH-F8AP6XoZrleihOjjAXROzEEp-mq7_xA"
+bot = telebot.TeleBot "7677423658:AAH-F8AP6XoZrleihOjjAXROzEEp-mq7_xA"
 
 prediction_history = deque(maxlen=5)
 
